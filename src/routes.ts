cc7cbd7f4e015eedccdd1f2b4c676591d2c6a103
raja-bala <bharathiraja.techie@gmail.com>
2025-07-20 22:16:54 +0800

    Adding Index Route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   // * matches all URLs, the ? makes it optional so it will match / as well
